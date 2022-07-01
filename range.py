18609ddac608313e874e42624c38ee56a959cf86
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-01 23:24:28 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,42 @@
+# for i in range(7):
+#     print(i)
+
+# n=range(9)
+# for i in n:
+#     print(i)
+
+# for w in range(1,5):
+#     print(w)
+
+# for r in range(4,5):
+#     print(r)
+
+# for t in range(7,7):
+#     print(t)
+
+# for n in range(0,10,1):
+#     print(n)
+
+# for n in range(0,10,2):
+#     print(n)
+
+# for n in range(1,10,2):
+#     print(n)
+
+# for n in range(2,10,2):
+#     print(n)
+
+# for n in range(10,0,-1):
+#     print(n)
+
+# for n in range(10,1,-1):
+#     print(n)
+
+# for i in range(10,1,-2):
+#     print(i)
+
+# for odd in range(1,50,2):
+#     print(odd,end=' ')
+
+# k=[1,2,3,4]
+# print(*k)
